931ca256494d0ea144e67e5b5f656ea3ee7a879b
tghaefli <haefliger.p@posteo.de>
2016-02-26 00:04:33 +0100

    assignments of SW1 complete

--- uebung01/Histogramm.m
+++ uebung01/Histogramm.m
@@ -6,18 +6,31 @@ Image = imread('London.png');
 
 %plot the image
 figure(1);
-subplot(1,2,1);
+subplot(2,2,1);
 imshow(Image);
 title('London.png');
 
 %get histogram values
 [Count, Value] = imhist(Image);
 %plot them
-subplot(1,2,2);
+grid on
+subplot(2,2,2);
 plot(Value, Count, 'bo-');
 xlabel('gray value');
 ylabel('absolute frequency')
 
 
-%??? relative and cumulative frequency
+% relative and cumulative frequency
+[sx, sy] = size(Image);
+Count2 = Count/(sx*sy);
+subplot(2,2,3);
+plot(Value, Count2, 'go-');
+xlabel('gray value');
+ylabel('relative frequency')
 
+% Histogram
+B = cumsum(Count2);
+subplot(2,2,4);
+plot(Value, B, 'r-');
+xlabel('gray value');
+ylabel('cumultative frequency')
