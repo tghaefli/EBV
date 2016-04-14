c23b90d77ee05d4eb75e30a2b2df732306d7ca26
tghaefli <haefliger.p@posteo.de>
2016-04-14 21:49:40 +0200

    changed musterloesung

--- testat01/testat_Lsg.m
+++ testat01/testat_Lsg.m
@@ -10,6 +10,9 @@ NumFiles = 15;
 
 NumBins = 4;
 
+map=colormap(jet);
+map(1,:) = 0;
+
 for i0 = 1:NumFiles
     ImageRead = imread(strcat(Folder, '\Image_', sprintf('%03d',i0),'.png'));
     
@@ -62,32 +65,35 @@ for i0 = 1:NumFiles
     %determine the angle (atan2 gives back the whole interval ]-pi , pi[ )
     Angle = atan2(ImageDy, ImageDx);
     Angle(Angle < 0) = pi+Angle(Angle < 0);
-    %Angle = pi/2+atan(ImageDy./min(ImageDx,1));
-    %use only those values that are above a given threshold
-    if UseSkelet == 1
-        Angle(~ImageSkel) = 0;
-    else
-        Angle(~ImageClose) = 0;
-    end   
-
+    
     %do binning (do not make difference between positive and negative
     %values)
+    %test using:
+    %NumBins = 6;Angle = [0:0.1:pi];plot(Angle,1+mod(round(Angle/(pi/NumBins)),NumBins),'bo-')
     BinVal = 1+mod(round(Angle/(pi/NumBins)),NumBins);
+    
+    %use only those values that are above a given threshold (values below threshold are treated as angle zero)
+    if UseSkelet == 1
+        Angle(~ImageSkel) = 0;
+        BinVal(~ImageSkel) = 0;
+    else
+        Angle(~ImageClose) = 0;
+        BinVal(~ImageClose) = 0;
+    end   
+
     AnzVal = [];
     for i1 = 1:NumBins
-        AnzVal = [AnzVal, numel(find(ImageSkel & BinVal == i1))];
+        AnzVal = [AnzVal, numel(find(BinVal == i1))];
     end
     [maxv, ind] = max(AnzVal);
  
     %plot it
     figure(5)
-    imshow(Angle, []);
-    map=colormap(jet);
-    map(1,:) = 0;
+    imshow(Angle, []);    
     colormap(map)
     title(strcat('angle: ', num2str((ind-1)*45), '°'));
     colorbar;
     
     %wait for one second (chose empty '' to wait till key is hit)
-    pause(1);
+    pause();
 end
