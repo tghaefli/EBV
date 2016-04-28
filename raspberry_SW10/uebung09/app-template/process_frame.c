1e44100acc326ef60231ddefd6256eb53c0031b6
tghaefli <haefliger.p@posteo.de>
2016-04-28 20:35:25 +0200

    started with uebung10

--- raspberry_SW10/uebung09/app-template/process_frame.c
+++ raspberry_SW10/uebung09/app-template/process_frame.c
@@ -15,12 +15,11 @@
 
 #define IMG_SIZE NUM_COLORS*OSC_CAM_MAX_IMAGE_WIDTH*OSC_CAM_MAX_IMAGE_HEIGHT
 
+/*
+ *   CONSTANTS
+ */
 const int nc = OSC_CAM_MAX_IMAGE_WIDTH;
 const int nr = OSC_CAM_MAX_IMAGE_HEIGHT;
-
-int TextColor;
-
-float bgrImg[IMG_SIZE];
 
 const float avgFac = 0.95;
 
@@ -33,8 +32,21 @@ const int frgLimit = 100;
 /* minimum size of objects (sum of all pixels) */
 const int MinArea = 500;
 
+/*
+ * VARIABLES
+ */
+int TextColor;
+
+float bgrImg[IMG_SIZE];
+
 struct OSC_VIS_REGIONS ImgRegions;/* these contain the foreground objects */
 
+int16 imgDx[IMG_SIZE];
+int16 imgDy[IMG_SIZE];
+
+/*
+ *  Prototypes
+ */
 void ChangeDetection();
 void SetBackground();
 void Erode_3x3(int InIndex, int OutIndex);
@@ -44,138 +56,157 @@ void DrawBoundingBoxes();
 
 void ResetProcess()
 {
-	SetBackground();
+    SetBackground();
 }
 
 
 void ProcessFrame()
 {
-	//initialize counters
-	if(data.ipc.state.nStepCounter == 1) {
-		SetBackground();
+    //initialize counters
+    if(data.ipc.state.nStepCounter == 1)
+    {
+        SetBackground();
 
-	} else {
+    }
+    else
+    {
 
-		ChangeDetection();
+        ChangeDetection();
 
-		Erode_3x3(THRESHOLD, INDEX0);
-		Dilate_3x3(INDEX0, THRESHOLD);
+        Erode_3x3(THRESHOLD, INDEX0);
+        Dilate_3x3(INDEX0, THRESHOLD);
 
-		DetectRegions();
-
-		DrawBoundingBoxes();
-	}
+        DetectRegions();
+        DrawBoundingBoxes();
+    }
 }
 
-void ChangeDetection() {
-	int r, c;
-	//set result buffer to zero
-	memset(data.u8TempImage[THRESHOLD], 0, IMG_SIZE);
+void ChangeDetection()
+{
+    int r, c;
+    //set result buffer to zero
+    memset(data.u8TempImage[THRESHOLD], 0, IMG_SIZE);
 
-	//loop over the rows
-	for(r = Border*nc; r < (nr-Border)*nc; r += nc) {
-		//loop over the columns
-		for(c = Border; c < (nc-Border); c++) {
-			float pImg = data.u8TempImage[SENSORIMG][r+c];
-			float pBgr = bgrImg[r+c];
-			float Dif = fabs(pImg-pBgr);
+    //loop over the rows
+    for(r = Border*nc; r < (nr-Border)*nc; r += nc)
+    {
+        //loop over the columns
+        for(c = Border; c < (nc-Border); c++)
+        {
+            unsigned char* p = &data.u8TempImage[SENSORIMG][r+c];
 
-			//if the difference is larger than threshold value (can be changed on web interface)
-			if(Dif > data.ipc.state.nThreshold) {
-				//set pixel value to 255 in BACKGROUND image (only the blue plane)
-				data.u8TempImage[THRESHOLD][r+c] = 255;
-				//increase foreground counter
-				data.u8TempImage[INDEX1][r+c]++;
-				//check whether limit is reached
-				if(data.u8TempImage[INDEX1][r+c] == frgLimit) {
-					//set pixel to background
-					bgrImg[r+c] = (float) data.u8TempImage[SENSORIMG][r+c];
-					data.u8TempImage[INDEX1][r+c] = 0;
-				}
-			} else {
-				// update background image
-				bgrImg[r+c] = avgFac*bgrImg[r+c] + (1-avgFac)*(float) data.u8TempImage[SENSORIMG][r+c];
-				// set value for display
-				data.u8TempImage[BACKGROUND][r+c] = (unsigned char) bgrImg[r+c];
-				//set foreground counter to zero
-				data.u8TempImage[INDEX1][r+c] = 1;
-			}
-		}
-	}
+            /* implement Sobel filter in x-direction */
+            int32 dx = -(int32) *(p-nc-1) + 0 + (int32) *(p-nc+1)
+                       -2* (int32) *(p-1) + 0 + 2* (int32) *(p+1)
+                       -(int32) *(p+nc-1) + 0 + (int32) *(p+nc+1);
+
+            int32 dy = -(int32) *(p-nc-1)  -  2* (int32) *(p-nc)  - (int32) *(p-nc+1)
+                       +         0         +        0             + 0
+                       + (int32) *(p+nc-1) +  2* (int32) *(p+nc)  + (int32) *(p+nc+1);  // TODO (pascal#1#): add filter ...
+
+            /* check if norm is larger than threshold */
+            int32 df2 = dx*dx+dy*dy;
+            int32 thr2 = data.ipc.state.nThreshold*data.ipc.state.nThreshold;
+            if(df2 > thr2)  //avoid square root
+            {
+                //set pixel value to 255 in THRESHOLD image for gui
+                data.u8TempImage[THRESHOLD][r+c] = 255;
+            }
+
+            //store derivatives (int16 is enough)
+            imgDx[r+c] = (int16) dx;
+            imgDy[r+c] = (int16) dy;
+
+            //possibility to visualize data
+            data.u8TempImage[BACKGROUND][r+c] = (uint8) MAX(0, MIN(255,128+(dy+dx)/2));
+        }
+    }
 }
 
 
-void SetBackground() {
-	int r, c;
+void SetBackground()
+{
+    int r, c;
 
-	//loop over the rows
-	for(r = Border*nc; r < (nr-Border)*nc; r += nc) {
-		//loop over the columns
-		for(c = Border; c < (nc-Border); c++) {
-			bgrImg[r+c] = (float) data.u8TempImage[SENSORIMG][r+c];
-		}
-	}
-	//set all counters to zero
-	memset(data.u8TempImage[INDEX1], 0, IMG_SIZE);
+    //loop over the rows
+    for(r = Border*nc; r < (nr-Border)*nc; r += nc)
+    {
+        //loop over the columns
+        for(c = Border; c < (nc-Border); c++)
+        {
+            bgrImg[r+c] = (float) data.u8TempImage[SENSORIMG][r+c];
+        }
+    }
+    //set all counters to zero
+    memset(data.u8TempImage[INDEX1], 0, IMG_SIZE);
 }
 
 
 void Erode_3x3(int InIndex, int OutIndex)
 {
-	int c, r;
+    int c, r;
 
-	for(r = Border*nc; r < (nr-Border)*nc; r += nc) {
-		for(c = Border; c < (nc-Border); c++) {
-			unsigned char* p = &data.u8TempImage[InIndex][r+c];
-			data.u8TempImage[OutIndex][r+c] = *(p-nc-1) & *(p-nc) & *(p-nc+1) &
-											   *(p-1)    & *p      & *(p+1)    &
-											   *(p+nc-1) & *(p+nc) & *(p+nc+1);
-		}
-	}
+    for(r = Border*nc; r < (nr-Border)*nc; r += nc)
+    {
+        for(c = Border; c < (nc-Border); c++)
+        {
+            unsigned char* p = &data.u8TempImage[InIndex][r+c];
+            data.u8TempImage[OutIndex][r+c] = *(p-nc-1) & *(p-nc) & *(p-nc+1) &
+                                              *(p-1)    & *p      & *(p+1)    &
+                                              *(p+nc-1) & *(p+nc) & *(p+nc+1);
+        }
+    }
 }
 
 void Dilate_3x3(int InIndex, int OutIndex)
 {
-	int c, r;
+    int c, r;
 
-	for(r = Border*nc; r < (nr-Border)*nc; r += nc) {
-		for(c = Border; c < (nc-Border); c++) {
-			unsigned char* p = &data.u8TempImage[InIndex][r+c];
-			data.u8TempImage[OutIndex][r+c] = *(p-nc-1) | *(p-nc) | *(p-nc+1) |
-											        *(p-1)    | *p      | *(p+1)    |
-											        *(p+nc-1) | *(p+nc) | *(p+nc+1);
-		}
-	}
+    for(r = Border*nc; r < (nr-Border)*nc; r += nc)
+    {
+        for(c = Border; c < (nc-Border); c++)
+        {
+            unsigned char* p = &data.u8TempImage[InIndex][r+c];
+            data.u8TempImage[OutIndex][r+c] = *(p-nc-1) | *(p-nc) | *(p-nc+1) |
+                                              *(p-1)    | *p      | *(p+1)    |
+                                              *(p+nc-1) | *(p+nc) | *(p+nc+1);
+        }
+    }
 }
 
 
-void DetectRegions() {
-	struct OSC_PICTURE Pic;
-	int i;
+void DetectRegions()
+{
+    struct OSC_PICTURE Pic;
+    int i;
 
-	//set pixel value to 1 in INDEX0 because the image MUST be binary (i.e. values of 0 and 1)
-	for(i = 0; i < IMG_SIZE; i++) {
-		data.u8TempImage[INDEX0][i] = data.u8TempImage[THRESHOLD][i] ? 1 : 0;
-	}
+    //set pixel value to 1 in INDEX0 because the image MUST be binary (i.e. values of 0 and 1)
+    for(i = 0; i < IMG_SIZE; i++)
+    {
+        data.u8TempImage[INDEX0][i] = data.u8TempImage[THRESHOLD][i] ? 1 : 0;
+    }
 
-	//wrap image INDEX0 in picture struct
-	Pic.data = data.u8TempImage[INDEX0];
-	Pic.width = nc;
-	Pic.height = nr;
-	Pic.type = OSC_PICTURE_BINARY;
+    //wrap image INDEX0 in picture struct
+    Pic.data = data.u8TempImage[INDEX0];
+    Pic.width = nc;
+    Pic.height = nr;
+    Pic.type = OSC_PICTURE_BINARY;
 
-	//now do region labeling and feature extraction
-	OscVisLabelBinary( &Pic, &ImgRegions);
-	OscVisGetRegionProperties( &ImgRegions);
+    //now do region labeling and feature extraction
+    OscVisLabelBinary( &Pic, &ImgRegions);
+    OscVisGetRegionProperties( &ImgRegions);
 }
 
 
-void DrawBoundingBoxes() {
-	uint16 o;
-	for(o = 0; o < ImgRegions.noOfObjects; o++) {
-		if(ImgRegions.objects[o].area > MinArea) {
-			DrawBoundingBox(ImgRegions.objects[o].bboxLeft, ImgRegions.objects[o].bboxTop,
-							ImgRegions.objects[o].bboxRight, ImgRegions.objects[o].bboxBottom, false, GREEN);
-		}
-	}
+void DrawBoundingBoxes()
+{
+    uint16 o;
+    for(o = 0; o < ImgRegions.noOfObjects; o++)
+    {
+        if(ImgRegions.objects[o].area > MinArea)
+        {
+            DrawBoundingBox(ImgRegions.objects[o].bboxLeft, ImgRegions.objects[o].bboxTop,
+                            ImgRegions.objects[o].bboxRight, ImgRegions.objects[o].bboxBottom, false, GREEN);
+        }
+    }
 }
